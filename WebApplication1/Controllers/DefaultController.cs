b4a0532fb03bb5466a364f28badffcdcdaddd978
Administrator <Administrator@OS-20200101ZZBL>
2020-04-13 12:20:53 +0800

    CCCCCCCCCCCC

--- WebApplication1/Controllers/DefaultController.cs
+++ WebApplication1/Controllers/DefaultController.cs
@@ -17,5 +17,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult BBBBBBBB()
+        {
+            return View();
+        }
     }
 }
